c36c981d2383c016642b02e9e10bb0dd6c7859e7
Goncalo Bernardo <gb@Goncalos-MBP.lan>
2016-07-30 15:49:44 +0100

    double quotes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const db = require("./db");
 
 app.use(bodyParser.json());
